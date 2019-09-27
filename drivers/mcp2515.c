f7353d7a1462993489ed43fa88072af2085c9ed9
Audun Asdal <audun.asdal@gmail.com>
2019-09-27 13:06:22 +0200

    MCP implementation

--- drivers/mcp2515.c
+++ drivers/mcp2515.c
@@ -2,5 +2,94 @@
  * mcp2515.c
  *
  * Created: 27.09.2019 11:18:14
- *  Author: Ole Sivert
+ *  Author: Audun
  */ 
+
+
+#include "mcp2515.h"
+#include "spi.h"
+#include <stdlib.h>
+//void spi_sendData(char *cData, int cDataLen);
+
+
+uint8_t mcp_read(int mode, uint8_t addr){
+	if (mode == MCP_MODE_CMD){
+		char arr[3] = {0x03, addr};
+		spi_sendData(arr, 3);
+		return arr[2];
+	}
+	char arr[2] = {0x90|addr<<1};
+	spi_sendData(arr, 2);
+	return arr[1];
+}
+
+void mcp_read_array(int mode, uint8_t addr, uint8_t *arr, uint8_t len){
+	if (mode == MCP_MODE_CMD) {
+		char *buff = malloc(sizeof(char) * (len + 2));
+		buff[0] = 0x03;
+		buff[1] = addr;
+		spi_sendData(buff, len + 2);
+		for (int i = 0; i < len; i++) {
+			arr[i] = buff[i+2];
+		}
+		free(buff);
+		return;
+	}
+	char *buff = malloc(sizeof(char) * (len + 1));
+	buff[0] = 0x90|addr<<1;
+	spi_sendData(buff, len + 1);
+	for (int i = 0; i < len; i++) {
+		arr[i] = buff[i+1];
+	}
+	free(buff);
+}
+
+uint8_t mcp_read_status(){
+	char arr[3] = {0xc0};
+	spi_sendData(arr, 3);
+	return arr[1];
+}
+
+void mcp_write(int mode, uint8_t addr, uint8_t data){
+	if (mode == MCP_MODE_CMD){
+		char arr[3] = {0x02, addr, data};
+		spi_sendData(arr, 3);
+		return;
+	}
+	char arr[2] = {0x80|addr, data};
+	spi_sendData(arr, 2);
+}
+
+void mcp_write_array(int mode, uint8_t addr, uint8_t *arr, uint8_t len) {
+	if (mode == MCP_MODE_CMD) {
+		char *buff = malloc(sizeof(char) * (len + 2));
+		for (int i = 0; i < len; i++) {
+			buff[i+2] = arr[i];
+		}
+		buff[0] = 0x02;
+		buff[1] = addr;
+		spi_sendData(buff, len + 2);
+		free(buff);
+		return;
+	}
+	char *buff = malloc(sizeof(char) * (len + 1));
+	for (int i = 0; i < len; i++) {
+		buff[i+1] = arr[i];
+	}
+	buff[0] = 0x80|addr;
+	spi_sendData(buff, len + 1);
+	free(buff);
+}
+
+void mcp_rts(int tx_buffer){
+	char command = 0x80|1<<tx_buffer;
+	spi_sendData(&command, 1);
+}
+void mcp_reset(){
+	char rst = 0xc0;
+	spi_sendData(&rst, 1);
+}
+void mcp_bit_modify(uint8_t mask, uint8_t addr, uint8_t data){
+	char arr[4]={0x05, addr, mask, data};
+	spi_sendData(arr, 4);
+}
--- drivers/mcp2515.h
+++ drivers/mcp2515.h
@@ -9,13 +9,17 @@
 #ifndef MCP2515_H_
 #define MCP2515_H_
 
+#include <avr/io.h>
+
 #define MCP_MODE_CMD 0
 #define MCP_MODE_CAN 1
 
 uint8_t mcp_read(int mode, uint8_t addr);
 uint8_t mcp_read_status();
+void mcp_read_array(int mode, uint8_t addr, uint8_t *arr, uint8_t len);
 void mcp_write(int mode, uint8_t addr, uint8_t data);
-void mcp_rts();
+void mcp_write_array(int mode, uint8_t addr, uint8_t *arr, uint8_t len);
+void mcp_rts(int tx_buffer);
 void mcp_reset();
 void mcp_bit_modify(uint8_t mask, uint8_t addr, uint8_t data);
 
